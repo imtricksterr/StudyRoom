6257b10d1dd713f979c5738cc5b7ae5c2fc0c4d7
triks <triks@DESKTOP-FQFOR7A>
2025-12-16 02:00:18 -0500

    Add project files.

---
+++ StudyRoom/StudyRoom.py
@@ -0,0 +1,37 @@
+# https://uconncalendar.lib.uconn.edu/reserve/GroupStudyRooms
+
+
+
+class Reservationist():
+
+
+    def main(self): pass
+
+    # Takes Students schedule for each day
+    # Takes Students login
+    # Checks for the Student's schedule
+
+
+
+
+    def check_date(self):
+        """ Checks current date on startup of program to see if the new study rooms are posted, runs the rest of the program after this requirement is met"""
+        pass
+
+    def student_info(self):
+        """ Takes students information for making reservations """ 
+        pass
+
+
+
+
+    def find_selections(self):
+        """ Uses student schedule to find reservations that work for them; we are taking this approach because depending on the week/day we may not be able to get the same exact room(s)"""
+        pass
+
+
+
+
+    def submit_res(self):
+        """ Uses student log in and schedule in order to make selected reservations for the student"""
+        pass
