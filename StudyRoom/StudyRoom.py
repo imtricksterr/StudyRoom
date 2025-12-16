e8d3459be2b46a580cf86ac5682fbf3d7df21480
tricksterr <jalenbell2017@gmail.com>
2025-12-16 04:38:37 -0500

    More Policy Adjustments

--- StudyRoom/StudyRoom.py
+++ StudyRoom/StudyRoom.py
@@ -13,19 +13,19 @@ class Reservationist:
 
 
     def student_schedule(self):
-        """ Takes students information for making reservations """ 
+        """ Takes students schedule for making reservations """ 
         pass
 
 
 
 
     def find_selections(self):
-        """ Uses student schedule to find reservations that work for them; we are taking this approach because depending on the week/day we may not be able to get the same exact room(s)"""
+        """ Uses student schedule to find reservations that work for them"""
         pass
 
 
 
 
     def submit_res(self):
-        """ Uses student log in and schedule in order to make selected reservations for the student"""
+        """ Uses student schedule to make selected reservations for the student"""
         pass
