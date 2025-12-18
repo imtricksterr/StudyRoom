5746b4bb60cb02004383d35527703d0e1929c7c4
tricksterr <jalenbell2017@gmail.com>
2025-12-17 20:40:22 -0500

    Elementary implementations of certain methods

--- StudyRoom/StudyRoom.py
+++ StudyRoom/StudyRoom.py
@@ -3,23 +3,29 @@
 
 # Adjusting in order to follow the UCONN IT policies on automation and web scraping.
 
-from playwright.sync_api, import sync_playwright
+from playwright.sync_api import sync_playwright
 
 class Reservationist:
 
+    playwright = sync_playwright().start()
 
     def main(self):
-       """ Main function to run the reservation process """
+        """ Main function to run the reservation process """
+        
         with sync_playwright() as p:
+            self.student_schedule()
             browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto("https://uconncalendar.lib.uconn.edu/reserve/GroupStudyRooms")
+
 
 
 
     def student_schedule(self):
         """ Takes students schedule for making reservations """ 
-        pass
-
-
+        day = input("Enter day you want a study room: ")
+        start_time = input("What time do you want to start studying")
+        end_time = input("What time do you want to stop studying")
 
 
     def find_selections(self):
