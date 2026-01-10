5ec7a839192edb933fec5b49ff57f8ae51fe71c0
tricksterr <jalenbell2017@gmail.com>
2026-01-09 23:46:26 -0500

    testing to see if locator finally can see these cells

--- StudyRoom/test_playwright.py
+++ StudyRoom/test_playwright.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=100)
+    browser = p.chromium.launch(headless=False, slow_mo=1000)
     page = browser.new_page()
     page.goto("https://uconncalendar.lib.uconn.edu/reserve/GroupStudyRooms")
-    page.get_by_role("button", name="Continue").click()
-    page.get_by_role("button", name="Submit my Booking").click()
-    
+    events = page.locator("a.fc-event")
+    print(events.count())
+    available = page.locator("a.fc-event:not(.fc-event-unavailable)")
+    print(available.count())
